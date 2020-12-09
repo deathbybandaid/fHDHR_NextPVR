186a6de6462a1a4c6aa3d12805deb7f0b50f2a80
deathbybandaid <github@deathbybandaid.net>
2020-12-09 14:15:24 -0500

    test

--- alternative_epg/tvtv.py
+++ alternative_epg/tvtv.py
@@ -58,8 +58,8 @@ class tvtvEPG():
 
     def get_cached(self, dates_to_pull):
         for datesdict in dates_to_pull:
-            starttime = "%sT00%3A00%3A00.000Z" % (datesdict["start"])
-            stoptime = "%sT00%3A00%3A00.000Z" % (datesdict["stop"])
+            starttime = "%sT00%3A00%3A00.000Z" % str(datesdict["start"])
+            stoptime = "%sT00%3A00%3A00.000Z" % str(datesdict["stop"])
             url = "https://www.tvtv.us/tvm/t/tv/v4/lineups/%s/listings/grid?start=%s&%s" % (self.lineup_id, starttime, stoptime)
             self.get_cached_item(str(starttime), url)
         cache_list = self.fhdhr.db.get_cacheitem_value("cache_list", "offline_cache", "tvtv") or []
