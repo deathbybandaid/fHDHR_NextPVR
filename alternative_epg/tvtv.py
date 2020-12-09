ce659099547cb1b462c6f357656f5c87b1da890a
deathbybandaid <github@deathbybandaid.net>
2020-12-09 13:15:20 -0500

    test

---
+++ alternative_epg/tvtv.py
@@ -0,0 +1,40 @@
+
+
+from fHDHR.exceptions import EPGSetupError
+
+
+class tvtvEPG():
+
+    def __init__(self, fhdhr, channels):
+        self.fhdhr = fhdhr
+
+        self.channels = channels
+
+    @property
+    def postalcode(self):
+        if self.fhdhr.config.dict["tvtv"]["postalcode"]:
+            return self.fhdhr.config.dict["tvtv"]["postalcode"]
+        try:
+            postalcode_url = 'http://ipinfo.io/json'
+            postalcode_req = self.fhdhr.web.session.get(postalcode_url)
+            data = postalcode_req.json()
+            postalcode = data["postal"]
+        except Exception as e:
+            raise EPGSetupError("Unable to automatically optain postalcode: " + str(e))
+            postalcode = None
+        return postalcode
+
+    @property
+    def lineup_id(self):
+        lineup_id_url = "https://www.tvtv.us/tvm/t/tv/v4/lineups?postalCode=%s" % self.postalcode
+        if self.fhdhr.config.dict["tvtv"]["lineuptype"]:
+            lineup_id_url += "&lineuptype=%s"
+        lineup_id_req = self.fhdhr.web.session.get(lineup_id_url)
+        data = lineup_id_req.json()
+        lineup_id = data[0]["lineupID"]
+        return lineup_id
+
+    def update_epg(self):
+        programguide = {}
+        print(self.lineup_id)
+        return programguide
